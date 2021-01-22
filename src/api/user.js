2666742ba99cfdbe04c6f3101dfcd3c809d5f800
caiwenglong <836472117@qq.com>
2021-01-22 18:08:02 +0800

    Revert "生产环境使用远程api进行请求数据"
    This reverts commit b99b4254

--- src/api/user.js
+++ src/api/user.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function apiLogin(data) {
   return request({
-    url: 'http://120.24.144.34:8803/user-center/user/login',
+    url: '/user-api/user-center/user/login',
     method: 'post',
     data
   })
@@ -10,7 +10,7 @@ export function apiLogin(data) {
 
 export function apiGetUserInfo(token) {
   return request({
-    url: 'http://120.24.144.34:8803/user-center/user/getUserInfos',
+    url: '/user-api/user-center/user/getUserInfos',
     method: 'get',
     params: { token }
   })
@@ -18,7 +18,7 @@ export function apiGetUserInfo(token) {
 
 export function apiLogout() {
   return request({
-    url: 'http://120.24.144.34:8803/user-center/user/logout',
+    url: '/user-api/user-center/user/logout',
     method: 'post'
   })
 }
